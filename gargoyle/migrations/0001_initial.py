ecfe0149be6a99e9cad6c3f460b0d2d935746167
Chris Bailey <cbailey@rldatix.com>
2025-04-03 15:33:17 -0400

    Updates for gargoyle

--- gargoyle/migrations/0001_initial.py
+++ gargoyle/migrations/0001_initial.py
@@ -1,5 +1,4 @@
 import django.utils.timezone
-import jsonfield.fields
 from django.db import migrations, models
 
 
@@ -13,7 +12,7 @@ class Migration(migrations.Migration):
             name='Switch',
             fields=[
                 ('key', models.CharField(primary_key=True, serialize=False, max_length=64)),
-                ('value', jsonfield.fields.JSONField()),
+                ('value', models.JSONField(default=dict)),
                 ('label', models.CharField(max_length=64, null=True)),
                 ('date_created', models.DateTimeField(default=django.utils.timezone.now)),
                 ('date_modified', models.DateTimeField(auto_now=True)),
